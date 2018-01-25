e076ed5b4a1ef725db6947aee3d117c431fb0d95
cahc <cristian.colliander@umu.se>
2018-01-25 14:22:19 +0100

    makeComparable half finished

--- src/RunThis.java
+++ src/RunThis.java
@@ -1,17 +1,35 @@
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 public class RunThis {
 
+    @Parameter(names = "-debug", description = "Debug mode")
+    private boolean debug = false;
 
+    @Parameter(names = "-help", help = true)
+    private boolean help;
 
+    public static void main(String[] args) {
+        RunThis runThis = new RunThis();
 
-    public static void main() {
+        JCommander jCommander = new JCommander(runThis);
 
+        jCommander.parse(args);
 
+        if(args.length == 0) {
 
+           jCommander.usage();
+
+        }
+
+        System.out.println(runThis.help);
 
 
     }
 
 
 
-
 }
+
+
+
