988b8b72e0ac599dceb277db35833d9a50d9ad22
cahc.ub <cristian.colliander@ub.umu.se>
2018-02-13 15:29:08 +0100

    saveToLinkedList

--- src/SimilarityValues/ECDF.java
+++ src/SimilarityValues/ECDF.java
@@ -52,7 +52,6 @@ public class ECDF implements Serializable{
     }
 
 
-
     public double getProbBinarySearch( double val) {
 
         //index of the search key, if it is contained in the array; otherwise, (-(insertion point) - 1). The insertion point is defined as the point at which the key would be inserted into the array: the index of the first element greater than the key, or a.length if all elements in the array are less than the specified key. Note that this guarantees that the return value will be >= 0 if and only if the key is found.
