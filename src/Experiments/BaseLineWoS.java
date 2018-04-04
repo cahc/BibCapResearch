c4520855ff17cf0caa9ccec74ad5c77f1afeeeea
cahc.ub <cristian.colliander@ub.umu.se>
2018-04-04 14:05:17 +0200

    added wos ref calc.

--- src/Experiments/BaseLineWoS.java
+++ src/Experiments/BaseLineWoS.java
@@ -5,11 +5,16 @@ import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.h2.mvstore.MVMap;
 import org.h2.mvstore.MVStore;
 import org.h2.mvstore.type.ObjectDataType;
 
-import java.util.HashMap;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,7 +24,62 @@ import java.util.Map;
 public class BaseLineWoS {
 
 
-    public static void main(String[] arg) {
+    static class FieldNorm {
+
+        double citTimesFracSum = 0;
+        double fracSum = 0;
+
+        public FieldNorm() {}
+
+
+        public double getCitTimesFracSum() {
+            return citTimesFracSum;
+        }
+
+        public void addToCitTimesFracSum(double citTimesFracSum) {
+            this.citTimesFracSum += citTimesFracSum;
+        }
+
+        public double getFracSum() {
+            return fracSum;
+        }
+
+        public void addToFracSum(double fracSum) {
+            this.fracSum += fracSum;
+        }
+
+
+        public double getFieldNorm() {
+
+            return this.citTimesFracSum / this.fracSum;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            FieldNorm fieldNorm = (FieldNorm) o;
+
+            if (Double.compare(fieldNorm.citTimesFracSum, citTimesFracSum) != 0) return false;
+            return Double.compare(fieldNorm.fracSum, fracSum) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
+            temp = Double.doubleToLongBits(citTimesFracSum);
+            result = (int) (temp ^ (temp >>> 32));
+            temp = Double.doubleToLongBits(fracSum);
+            result = 31 * result + (int) (temp ^ (temp >>> 32));
+            return result;
+        }
+    }
+
+
+
+    public static void main(String[] arg) throws IOException {
 
         if(arg.length != 1) { System.out.println("Supply db"); System.exit(0); }
 
@@ -31,46 +91,131 @@ public class BaseLineWoS {
 
         MVMap<Integer, BibCapRecord> map = store.openMap("mymap", new MVMap.Builder<Integer, BibCapRecord>().keyType(new ObjectDataType()).valueType(new BibCapRecord()));
 
-        IntList TC_exlSelfCit = new IntArrayList();
-        IntList TC_incSelfCit = new IntArrayList();
 
         Object2IntMap<String> catToFreq = new Object2IntOpenHashMap<>();
+        Object2ObjectMap<String,FieldNorm> catToFieldNorm = new Object2ObjectOpenHashMap<>();
+        List<BibCapRecord> bibCapRecordList = new ArrayList<>();
 
 
+        //mappy.db only contains records with at least one WC
         for(Map.Entry<Integer, BibCapRecord> entry : map.entrySet()) {
 
 
-           List<String> categories = entry.getValue().getSubjectCategories();
+            bibCapRecordList.add(entry.getValue());
 
-           for(String s : categories) {
+        }
 
-
-               int occurences = catToFreq.getOrDefault(s,-1);
-               if(occurences == -1) { catToFreq.put(s, 1); } else {
-
-                   catToFreq.replace(s,occurences,occurences+1);
-
-               }
+        store.close();
 
 
-           }
+        for(BibCapRecord record : bibCapRecordList) {
+
+            List<String> categories = record.getSubjectCategories();
+            int TC_exlSelfCit = record.getCitationsExclSelf();
+            int n_cat = categories.size();
+
+            for(String s : categories) {
 
 
+                int occurences = catToFreq.getOrDefault(s,-1);
+
+                if(occurences == -1) { catToFreq.put(s, 1);  FieldNorm newFieldNorm = new FieldNorm(); newFieldNorm.addToCitTimesFracSum( TC_exlSelfCit* (1/((double)n_cat)) );  newFieldNorm.addToFracSum((1/((double)n_cat))); catToFieldNorm.put(s,newFieldNorm);  } else {
+
+                    catToFreq.replace(s,occurences,occurences+1);
+
+                    FieldNorm oldFieldNorm = catToFieldNorm.get(s);
+                    oldFieldNorm.addToCitTimesFracSum(   TC_exlSelfCit* (1/((double)n_cat))  );
+                    oldFieldNorm.addToFracSum( (1/((double)n_cat))  );
+
+                }
+
+            }
 
         }
 
 
-        System.out.println("# keys: " + catToFreq.size());
+
+        BufferedWriter writer1 = new BufferedWriter( new OutputStreamWriter(new FileOutputStream(new File("WoS_Records_Per_Category.txt")), StandardCharsets.UTF_8) );
+
+        System.out.println("calculating records per ctegory (non frac)");
 
         for(Object2IntMap.Entry<String> set : catToFreq.object2IntEntrySet() ) {
 
-            System.out.println( set.getKey() + " : " + set.getIntValue() );
+         writer1.write( set.getKey() + "\t" + set.getIntValue() );
+        writer1.newLine();
+
+        }
+
+        writer1.flush();
+        writer1.close();
+
+        System.out.println("calculating field norms based on WoS");
+
+        BufferedWriter writer0 = new BufferedWriter( new OutputStreamWriter(new FileOutputStream(new File("WoS_based_FieldValues.txt")), StandardCharsets.UTF_8) );
+
+        for(Map.Entry<String,FieldNorm> entry : catToFieldNorm.entrySet()) {
+
+
+            writer0.write(entry.getKey() + "\t" + entry.getValue().getFieldNorm());
+            writer0.newLine();
 
 
         }
 
+        writer0.flush();
+        writer0.close();
 
-    }
+        System.out.println("calculating norms per doc with harmonic mean");
+
+        BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(new FileOutputStream(new File("WoS_based_refValues.txt")), StandardCharsets.UTF_8) );
+
+        for(BibCapRecord record : bibCapRecordList) {
+
+            List<String> categories = record.getSubjectCategories();
+            int TC_exlSelfCit = record.getCitationsExclSelf();
+            int n_cat = categories.size();
+
+            List<FieldNorm> fieldNormList = new ArrayList<>();
+            List<Double> fieldNormValues = new ArrayList<>();
+
+            for(String s : categories) fieldNormList.add(catToFieldNorm.get(s));
+            for(FieldNorm fieldNorm : fieldNormList) fieldNormValues.add(fieldNorm.getFieldNorm());
+
+                   double finalFieldNorm = 0;
+                   boolean zeroValueFieldNorm = false;
+
+                   for(Double fieldValue : fieldNormValues) {
+
+                       if(fieldValue.equals(0D)) {
+
+                           zeroValueFieldNorm = true;
+                           break;
+
+                       }
+
+                     finalFieldNorm += (1/fieldValue);
+
+
+                    }
+
+                    if(zeroValueFieldNorm) { finalFieldNorm = -99; } else {
+
+                        finalFieldNorm = n_cat/finalFieldNorm;
+                    }
+
+
+                 writer.write(record.getUT() +"\t" + TC_exlSelfCit +"\t" + finalFieldNorm + "\t" +categories.toString() );
+                 writer.newLine();
+
+
+            }
+
+            writer.flush();
+            writer.close();
+
+        }
+
+
 
 
 }
